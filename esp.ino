9de8f76e49bf25f2f226f40d8189daac115d4755
bibishte <bibishte@gmail.com>
2018-03-12 19:45:33 +0200

    Fix padding
    Само малко оправихме падинга

--- esp.ino
+++ esp.ino
@@ -10,7 +10,7 @@ char buff[]="F\n";
 char buff1[]="S\n";
 
 String header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
-String html_1 = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'/><meta charset='utf-8'><style>body {font-size:140%;} #main {display: table; margin: auto;  padding: 0 10px 0 10px; } h2,{text-align:center; } .button { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;} .button1 { padding:100px 100px 100px 100px; width:100%;  background-color: #4CAF50; font-size: 120%;}</style><title>LED Control</title></head><body><div id='main'><h2>LED Control</h2>";
+String html_1 = "<!DOCTYPE html><html><head><meta name='viewport' content='width=device-width, initial-scale=1.0'/><meta charset='utf-8'><style>body {font-size:140%;} #main {display: table; margin: auto;  padding: 0 10px 0 10px; } h2,{text-align:center; } .button { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;} .button1 { padding:10px 10px 10px 10px; width:100%;  background-color: #4CAF50; font-size: 120%;}</style><title>LED Control</title></head><body><div id='main'><h2>LED Control</h2>";
 String html_2 = "<form id='F1' action='FORWARD'><input class='button' type='submit' value='move forward' ></form><br>";
 String html_3 = "<form id='F1' action='STOP'><input class='button1' type='submit' value='stop' ></form><br>";
 String html_4 = "</div></body></html>";
